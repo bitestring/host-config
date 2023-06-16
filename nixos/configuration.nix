796f63584dd8d96e2e6b24f4465561a51ac66c77
bitestring <81476430+bitestring@users.noreply.github.com>
2023-06-16 13:45:33 +0000

    Optimise Nix store disk usage

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -176,6 +176,16 @@
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
 
+  # Optimise Nix storage
+  nix.settings.auto-optimise-store = true;
+
+  # Automatic Garbage Collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
