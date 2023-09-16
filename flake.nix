162a2bb1bb6a084e9c41271d5e4c6e9877f4968f
bitestring <81476430+bitestring@users.noreply.github.com>
2023-09-16 10:51:27 +0530

    Add Nix Flake for development

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "bitestring.github.io";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      dependencies = with pkgs; [
+        # system deps
+
+        # Python deps
+        ansible
+        ansible-lint
+        ansible-language-server
+      ];
+
+    in {
+      # packages.${system}.default = pkgs.hello;
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = dependencies;
+      };
+    };
+}
