f385451b6d06bb55f01bcfcda2c8e2321b265209
bitestring <81476430+bitestring@users.noreply.github.com>
2023-12-22 12:28:11 +0530

    Cleanups

--- nixos-vm/flake.nix
+++ nixos-vm/flake.nix
@@ -14,7 +14,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
