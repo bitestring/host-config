d49af5c8b8c9561fb179e1f06a99714e4e3e7d5e
bitestring <81476430+bitestring@users.noreply.github.com>
2024-01-01 09:07:01 +0530

    Switch to nixpkgs-unstable for this repo

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "bitestring.github.io";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs }:
