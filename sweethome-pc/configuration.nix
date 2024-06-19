f906db3c4783fc90517c17ee97f0ce8f648a9c62
bitestring <81476430+bitestring@users.noreply.github.com>
2024-06-19 14:45:11 +0530

    Add nix configuration for personal PC

---
+++ sweethome-pc/configuration.nix
@@ -0,0 +1,252 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, inputs, host, user, hardware, ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  hardware.enableAllFirmware = true;
+
+  # Bootloader.
+  # boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/etc/secureboot";
+  };
+
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  fileSystems = {
+    "/".options = [ "compress=zstd" "relatime" ];
+    "/mnt/WD-SSD".options = [ "compress=zstd" "relatime" ];
+    "/mnt/samba/sweethome-data" = {
+      device = "//sweethome-server.lan/sweethome-data";
+      fsType = "cifs";
+      options =
+        let
+          # this line prevents hanging on network split
+          automount_opts = "_netdev,noauto,x-systemd.automount,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        in
+        [ "${automount_opts},uid=1000,gid=100,credentials=/home/${user.name}/.smbcred" ];
+    };
+  };
+
+  # Swap
+  zramSwap.enable = true;
+
+  networking.hostName = host.name; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_IN";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    # Configure keymap in X11
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Shell
+  programs.fish.enable = true;
+  environment.shells = with pkgs; [ fish ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${user.name} = {
+    isNormalUser = true;
+    description = user.description;
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+    shell = pkgs.fish;
+  };
+
+  # Enable automatic login for the user.
+  services.displayManager = {
+    autoLogin = {
+      enable = true;
+      user = user.name;
+    };
+  };
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  services.fwupd.enable = true;
+  services.fstrim.enable = true;
+  services.btrfs.autoScrub = {
+    enable = true;
+    fileSystems = [
+      "/"
+      "/mnt/WD-SSD"
+    ];
+  };
+  services.flatpak.enable = true;
+  services.cockpit.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
+    sbctl
+    cifs-utils
+    smartmontools
+    dig
+    git
+    veracrypt
+    gnome.gnome-software
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  services.syncthing = {
+    enable = true;
+    user = user.name;
+    dataDir = "/home/${user.name}/"; # Default folder for new synced folders
+    configDir = "/home/${user.name}/.config/syncthing"; # Folder for Syncthing's settings and keys
+  };
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+  };
+
+  # Open ports in the firewall.
+  networking.nftables.enable = true;
+  networking.firewall = {
+    enable = true;
+    checkReversePath = true;
+    interfaces = {
+      "wlo1" = {
+        allowedTCPPorts = [
+          22000
+        ];
+        allowedUDPPorts = [
+          22000
+          21027
+        ];
+      };
+      "virbr0" = {
+        allowedTCPPorts = [
+          22000
+        ];
+        allowedUDPPorts = [ 53 67 547 22000 21027 ]; # DNS, DHCP Server, DHCPv6 Server, Syncthing
+      };
+    };
+  };
+
+  networking.wg-quick.interfaces = {
+    "wg0".configFile = "/home/${user.name}/.wg0.conf";
+  };
+
+  # Auto system upgrade
+  system.autoUpgrade = {
+    # enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "--commit-lock-file"
+      "-L" # print build logs
+    ];
+    dates = "daily";
+    operation = "boot";
+  };
+
+  # Auto optimize Nix store during rebuild
+  nix.settings.auto-optimise-store = true;
+
+  # Automatic Garbage Collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
---
+++ sweethome-pc/home.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, inputs, host, user, ... }:
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = user.name;
+  home.homeDirectory = "/home/${user.name}";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Fonts
+  fonts.fontconfig.enable = true;
+
+  # User packages
+  home.packages = with pkgs; [
+    # gnome extensions
+    gnomeExtensions.dash-to-dock
+
+    # apps
+    tilix
+  ];
+
+  programs = {
+    home-manager.enable = true;
+    fish.enable = true;
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    git.enable = true;
+  };
+}
