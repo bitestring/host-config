b189cef867ff5b0969175d4bff955c39f300d185
bitestring <81476430+bitestring@users.noreply.github.com>
2024-07-16 13:17:33 +0530

    Remove deprecated option

--- nixos-vm/configuration.nix
+++ nixos-vm/configuration.nix
@@ -111,7 +111,6 @@
   # services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
