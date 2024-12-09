a30f3ac54e2b949b457dbf6a0ef34116ae6203a8
bitestring <81476430+bitestring@users.noreply.github.com>
2024-12-09 20:53:22 +0530

    Add luci-app-attendedsysupgrade to do painless OpenWrt upgrade

--- openwrt/packages.sh
+++ openwrt/packages.sh
@@ -1,1 +1,8 @@
-opkg update; opkg install tcpdump curl bind-dig dnscrypt-proxy2 adblock luci-app-adblock;
+opkg update
+opkg install tcpdump \
+    curl \
+    bind-dig \
+    dnscrypt-proxy2 \
+    adblock \
+    luci-app-adblock \
+    luci-app-attendedsysupgrade
