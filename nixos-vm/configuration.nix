0602db941f29413fc69c085bad31b2b3fe370d52
bitestring <81476430+bitestring@users.noreply.github.com>
2025-05-07 18:14:26 +0530

    Formatting

--- nixos-vm/configuration.nix
+++ nixos-vm/configuration.nix
@@ -90,22 +90,24 @@
   time.timeZone = "Asia/Kolkata";
 
   # Select internationalisation properties.
-  i18n.supportedLocales = [
-    "en_US.UTF-8/UTF-8"
-    "en_IN/UTF-8"
-  ];
-  i18n.defaultLocale = "en_IN.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ALL = "en_IN.UTF-8";
-    LC_ADDRESS = "en_IN.UTF-8";
-    LC_IDENTIFICATION = "en_IN.UTF-8";
-    LC_MEASUREMENT = "en_IN.UTF-8";
-    LC_MONETARY = "en_IN.UTF-8";
-    LC_NAME = "en_IN.UTF-8";
-    LC_NUMERIC = "en_IN.UTF-8";
-    LC_PAPER = "en_IN.UTF-8";
-    LC_TELEPHONE = "en_IN.UTF-8";
-    LC_TIME = "en_IN.UTF-8";
+  i18n = {
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "en_IN/UTF-8"
+    ];
+    defaultLocale = "en_IN.UTF-8";
+    extraLocaleSettings = {
+      LC_ALL = "en_IN.UTF-8";
+      LC_ADDRESS = "en_IN.UTF-8";
+      LC_IDENTIFICATION = "en_IN.UTF-8";
+      LC_MEASUREMENT = "en_IN.UTF-8";
+      LC_MONETARY = "en_IN.UTF-8";
+      LC_NAME = "en_IN.UTF-8";
+      LC_NUMERIC = "en_IN.UTF-8";
+      LC_PAPER = "en_IN.UTF-8";
+      LC_TELEPHONE = "en_IN.UTF-8";
+      LC_TIME = "en_IN.UTF-8";
+    };
   };
 
   # Fonts
